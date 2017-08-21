bff3612149a349e05b5f7d6831edf246896e8645
Alex Quan <alxquan@gmail.com>
2017-08-20 18:14:02 -0600

    Trying to understand what (this) does

--- src/index.js
+++ src/index.js
@@ -3,17 +3,17 @@ import { render } from 'react-dom';
 import { BrowserRouter, Match, Miss } from 'react-router';
 import './css/style.css';
 import App from './components/App';
-import StorePicker from './components/StorePicker'
-import NotFound from './components/NotFound'
+import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
 
 const Root = () => {
   return(
     <BrowserRouter>
-    <div>
-      <Match exactly pattern="/" component={StorePicker} />
-      <Match pattern="/store/:storeId" component={App} />
-      <Miss component={NotFound}/>
-    </div>
+      <div>
+        <Match exactly pattern="/" component={StorePicker} />
+        <Match pattern="/store/:storeId" component={App} />
+        <Miss component={NotFound}/>
+      </div>
     </BrowserRouter>
   )
 }
